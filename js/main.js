aae6baceafda2fdff8872ea4186223fdbcb50787
IlariaGalliano <ilaria.galliano7@gmail.com>
2020-11-09 17:03:57 +0100

    vue-slider
    manca es.bonus js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,37 @@
 /**
  * Slider
-
-
- Gestire navigazione immagini con click su freccie e pallini
- Vi allego sotto le immagini dello slider ma potete tranquillamente usare le vostre se preferite
- Bonus: fare eseguire il loop delle immagini, cambiando immagine ogni 3 secondi
+ * Gestire navigazione immagini con click su freccie e pallini
+ * Bonus: fare eseguire il loop delle immagini, cambiando immagine ogni 3 secondi
  */
+
+const app = new Vue({
+  el: '#app',
+  data: {
+    indexPhoto: 0,
+    photos:[
+      './images/image1.jpg',
+      './images/image2.jpg',
+      './images/image3.jpg',
+      './images/image4.jpg'
+    ]
+  },
+  methods:{
+    nextPhoto(){
+      this.indexPhoto += 1
+
+      if (this.indexPhoto > (this.photos.length -1)) {
+        this.indexPhoto = 0;
+      }
+    },
+    prevPhoto(){
+      this.indexPhoto -= 1
+
+      if (this.indexPhoto < 0) {
+        this.indexPhoto = this.photos.length -1;
+      }
+    },
+    orderPhoto(index){
+      this.indexPhoto = index;
+    }
+  }
+});
