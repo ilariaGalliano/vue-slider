1e32dadee97207824c4127e29b149c3fcf95b11e
IlariaGalliano <ilaria.galliano7@gmail.com>
2020-11-09 16:06:44 +0100

    vue-slider
    html e css

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/**
+ * Slider
+
+
+ Gestire navigazione immagini con click su freccie e pallini
+ Vi allego sotto le immagini dello slider ma potete tranquillamente usare le vostre se preferite
+ Bonus: fare eseguire il loop delle immagini, cambiando immagine ogni 3 secondi
+ */
